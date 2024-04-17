014c371c584ff91e524ce1d3e6b64a301fc8c3bd
GBM1986 <gunnar_bjarni@hotmail.com>
2024-04-17 14:02:32 +0200

    asdas

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,6 @@ export const Login = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE3MTMzNDkzNTUsImF1ZCI6WyJodHRwczovL2FwaS1ldS13ZXN0LTIuaHlncmFwaC5jb20vdjIvY2x1dW94M2d1MGsxYjA3dXdsamozbW50bC9tYXN0ZXIiLCJtYW5hZ2VtZW50LW5leHQuZ3JhcGhjbXMuY29tIl0sImlzcyI6Imh0dHBzOi8vbWFuYWdlbWVudC1ldS13ZXN0LTIuaHlncmFwaC5jb20vIiwic3ViIjoiY2FmYjYwYjctMzE2My00OWEwLTkwZmQtMjk3OWQ0NWRkMGY5IiwianRpIjoiY2x2M255dmhrbWhlYzA3bXZnanVpMGY2NiJ9.klEUi3_fboyhksLwB0HmkF0zfvE_hCJuQYyyzrMdc3TXUNxobiyGcTpA72vmkDhG77hoQ8MAecr3NpOWTPU53Hxfl5Gf7y0azR8BrlXiFWJB6FTdYE_J_LbR6IwJfjBVnAnrBnBXb2d2rjfWftOq_HLV181AAikbN-lVtIDW6B_At9hF_VylbkhvIQxF_xFRvpvvGUb6-Q0BLObdC0dAwGe4sOi3QXE8dul32XDbbDLfpV_OXd0mfZrzj_29mBJz3mqpbNIIjHEEpnUo0din35x1s4SDrRF_k06fp0oj2nfqV3q6YmMNQ1x7dzSeIRIMEhyuFGf494eCq0Hv5jnyhaMWk2EeMrj012wPshJ0CE-u5tCR3Kk74NeBlSLIci2N9SwYlQTlrhTnAX6TezNV_wd2w1HkiwiWcL7FxsVLmU4HN7Wz87HM5a7QXNrAY-fRBreaj5hPlbGro0J0Be5F7JvydZs0KA728a-6TlPlrKnfRI1xFTOOFPir6XIuNtg4qLcrS1e0X_cr_MAZGx6pw7s_O6ItpqH7El5UroWmPSdYuQyGaYVeC33fjNMGeWrKMNVvRUNMqsI7trtGWTWdx53Vilbmhr9PTGyllM-e5fdpNVhAcZPZJP5Q34qVDqzh0097Lmq0icS8n2CE64iHQ4AlD8DoWuY2Wv1MZWoi7NQ',
         },
         body: JSON.stringify({ username, password }),
       });
